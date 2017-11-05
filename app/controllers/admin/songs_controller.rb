45cbf45e3345d30b4df6d8f0d7c805c07c9ec8d4
Lee Whittaker <whittakerlee81@gmail.com>
2017-11-05 14:28:51 +0000

    Add devise Users

---
+++ app/controllers/admin/songs_controller.rb
@@ -0,0 +1,5 @@
+class Admin::SongsController < AdminController
+  def index
+  
+  end
+end
--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module Choir
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    config.generators.stylesheets = false
+    config.generators.javascripts = false
   end
 end
