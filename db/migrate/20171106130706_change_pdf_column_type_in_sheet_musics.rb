1ca140ffbe3ca4dd359f07171a94b31031ddfcbf
Lee Whittaker <whittakerlee81@gmail.com>
2017-11-07 14:26:25 +0000

    Fix database migration error.

--- db/migrate/20171106130706_change_pdf_column_type_in_sheet_musics.rb
+++ db/migrate/20171106130706_change_pdf_column_type_in_sheet_musics.rb
@@ -1,7 +1,5 @@
 class ChangePdfColumnTypeInSheetMusics < ActiveRecord::Migration[5.1]
   def change
-    remove_column :sheet_musics, :pdf, :string
-    
     add_attachment :sheet_musics, :pdf
   end
 end
