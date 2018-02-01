9867be24aa2c8c27ffebc3fc1eee8a7704e68903
Lee Whittaker <whittakerlee81@gmail.com>
2018-02-01 18:47:09 +0000

    Update database calls based on database type

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,7 +4,14 @@ class SongsController < ApplicationController
     
     @years = Song.all.map{|i| i.performance_date.year }.uniq
     
-    @grouped_songs = Song.where("cast(strftime('%Y', performance_date) as int) = ?", @current_year)
+    case ActiveRecord::Base.connection.adapter_name 
+    when "SQLite"
+      sorter = "cast(strftime('%Y', performance_date) as int) = ?"
+    when "PostgreSQL"
+      sorter = "extract(year from performance_date) = ?"
+    end
+    
+    @grouped_songs = Song.where(sorter, @current_year)
                          .group_by(&:performance_date)
   end
 end
