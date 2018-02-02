10e03eb978024472534ac7d25806e3dbe2ad7838
Lee Whittaker <whittakerlee81@gmail.com>
2018-02-02 14:50:55 +0000

    Set tab content to be on one page without params

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,21 +1,7 @@
 class SongsController < ApplicationController
   def index
-    if params[:year].present?
-      @current_year = params[:year].to_i
-    else
-      @current_year = DateTime.now.year.to_i
-    end
-    
-    @years = Song.all.map{|i| i.performance_date.year }.uniq
-    
-    case ActiveRecord::Base.connection.adapter_name 
-    when "SQLite"
-      sorter = "cast(strftime('%Y', performance_date) as int) = ?"
-    when "PostgreSQL"
-      sorter = "extract(year from performance_date) = ?"
-    end
-    
-    @grouped_songs = Song.where(sorter, @current_year)
-                         .group_by(&:performance_date)
+    @songs_by_year = Song.all
+                         .group_by {|s| s.performance_date}
+                         .group_by {|d| d[0].year}
   end
 end
