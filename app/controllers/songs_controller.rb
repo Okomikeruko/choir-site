c1d7215424736d453714f12d2790cbd1d4d39c19
Lee Whittaker <whittakerlee81@gmail.com>
2018-03-21 00:28:39 +0000

    Set controller to sort and group songs by title, pending, and history.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,6 +4,18 @@ class SongsController < ApplicationController
   
   
   def index
+    @songs = Song.all 
+    @songs_by_title = @songs.sort_by {|s| s.sort_order}
+                            .group_by {|s| s.sort_order.chr}
+    @songs_pending = @songs.where("performance_date > :d", :d => DateTime.now)
+                           .sort_by {|s| s.performance_date}
+                           .group_by {|s| s.performance_date}
+                           .group_by {|d| d[0].year}
+    @song_history = @songs.where("performance_date <= :d", :d => DateTime.now)
+                           .sort_by {|s| s.performance_date}
+                           .reverse!
+                           .group_by {|s| s.performance_date}
+                           .group_by {|d| d[0].year}
     
     @songs_by_year = Song.all
                          .group_by {|s| s.performance_date}
