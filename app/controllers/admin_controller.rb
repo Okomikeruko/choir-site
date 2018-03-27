ed454355566d011852e870d62f22cda411b956d0
Lee Whittaker <whittakerlee81@gmail.com>
2018-03-27 17:55:40 +0000

    Cosmetic code fixes.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,9 +3,9 @@ class AdminController < ApplicationController
   before_action :authenticate_user!
   
   def index
-    @articles = Article.first(3)
-    @songs    = Song.last(5).reverse!
+    @articles     = Article.first(3)
     @performances = Performance.last(5).reverse!
-    @rehearsals = Rehearsal.last(3) 
+    @rehearsals   = Rehearsal.last(3) 
+    @songs        = Song.last(5).reverse!
   end
 end
