869dcce4c0c47cf53d1dbe7c365ebf268cd4dd46
Lee Whittaker <whittakerlee81@gmail.com>
2018-03-27 19:29:17 +0000

    Update migrations for heroku

--- db/migrate/20180325013129_create_rehersal_songs.rb
+++ db/migrate/20180325013129_create_rehersal_songs.rb
@@ -1,7 +1,7 @@
 class CreateRehersalSongs < ActiveRecord::Migration[5.1]
   def change
     create_table :rehersal_songs do |t|
-      t.references :rehersal, foreign_key: true
+      t.references :rehearsal, foreign_key: true
       t.references :song, foreign_key: true
 
       t.timestamps
--- db/migrate/20180325032555_rename_rehersal_songs.rb
+++
@@ -1,6 +0,0 @@
-class RenameRehersalSongs < ActiveRecord::Migration[5.1]
-  def change
-    rename_table :rehersal_songs, :rehearsal_songs
-    rename_column :rehearsal_songs, :rehersal_id, :rehearsal_id
-  end
-end
