ad18be5514668d65eada5638962247e905ec0559
Lee Whittaker <whittakerlee81@gmail.com>
2025-03-02 16:10:50 -0700

    Fix lint issues in migration

--- db/migrate/20250220162254_fix_instruments_name_index.rb
+++ db/migrate/20250220162254_fix_instruments_name_index.rb
@@ -2,8 +2,8 @@
 
 class FixInstrumentsNameIndex < ActiveRecord::Migration[6.1]
   def change
-    if index_exists?(:instruments, :name, name: 'index_instruments_on_lower_name_and_song')
-      remove_index :instruments, :name, name: 'index_instruments_on_lower_name_and_song'
-    end
+    return unless index_exists?(:instruments, :name, name: 'index_instruments_on_lower_name_and_song')
+
+    remove_index :instruments, :name, name: 'index_instruments_on_lower_name_and_song'
   end
 end
