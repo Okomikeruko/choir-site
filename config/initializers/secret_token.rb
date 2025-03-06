997ccede4baa98700e3370278c38087318dfc577
Lee Whittaker <whittakerlee81@gmail.com>
2025-03-06 10:05:00 -0700

    FIX rails 7 depreciation warnings with corrected credentials configuration.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Be sure to restart your server when you modify this file.
+
+# Configure the application's secret_key_base for verifying the integrity of signed cookies.
+Rails.application.config.secret_key_base = Rails.application.credentials.dig(Rails.env.to_sym, :secret_key_base)
+
+# Fall back to ENV variable for production if not found in credentials
+if Rails.env.production? && Rails.application.config.secret_key_base.nil?
+  Rails.application.config.secret_key_base = ENV.fetch("SECRET_KEY_BASE")
+end
