83f1809b4fda110c907dcc6dc8b57438bfc3c17d
Lee Whittaker <whittakerlee81@gmail.com>
2025-03-21 15:01:16 -0600

    Reconfigure redis and db config.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -14,9 +14,32 @@ import './channels';
 import './cable';
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Re-initialize bootstrap components after Turbo navigation
-    const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl)
-    });
+// Re-initialize bootstrap components after Turbo navigation
+  const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  });
+
+  const repeatButtons = document.querySelector('.bin-repeat');
+  repeatButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const id = this.dataset.id;
+      this.classList.toggle('active');
+
+      const audioElement = document.getElementById(id);
+      if (audioElement) {
+        audioElement.setAttribute(
+          'data-repeat',
+          this.classList.contains('active') ? 'true' : 'false'
+        );
+      }
+    })
+  })
 });
+
+function replay(audio) {
+  // Check if the audio element has the repeat attribute set to "true"
+  if (audio.getAttribute('data-repeat') === 'true') {
+    audio.play();
+  }
+}
